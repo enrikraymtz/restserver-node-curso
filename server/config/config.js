d273389040ddeb788b7eec95b68ae7d7e596700e
DESKTOP-QKLA90D\enrik <henrydraymundo@gmail.com>
2018-04-29 17:48:57 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlBD;
 if (process.env.NODE_ENV === "dev") {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb://cafe-user:123456@ds263619.mlab.com:63619/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLBD = urlBD;
