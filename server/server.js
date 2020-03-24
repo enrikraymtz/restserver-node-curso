c1e9a37239934acbcb1b29c26e125965cd1aeffe
henrydraymundo/ <henrydraymundo@gmail.com>
2020-03-24 17:14:16 -0600

    pruebas

--- server/server.js
+++ server/server.js
@@ -8,10 +8,9 @@ const path = require("path");
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
 // habilitar la carpeta public
